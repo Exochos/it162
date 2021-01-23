ecaccd3b5d8524774ec8bd0662817656609819e4
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-23 11:49:15 -0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -8,8 +8,8 @@
  <meta name="author" content="Jeremy Ward" />
  <meta name="robots" content="noindex,nofollow" />
  <meta name="viewport" content="width=device-width, initial-scale=1" />
- <link rel="stylesheet" href="../css/style.css" />
- <link rel="stylesheet" href="../css/form.css" />
+ <link rel="stylesheet" href="css/style.css" />
+ <link rel="stylesheet" href="css/form.css" />
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
  <script src='js/main.js'></script>

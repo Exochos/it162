7b380b3e403a39be5d56695088f70300a3017396
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-23 18:36:19 -0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <!-- Meta information we are also using a cdn to get jquery-->
- <title> <?=$title?></title>
- <meta charset="utf-8" />    
- <meta name="description" content="Jeremy Wards Portal For IT162" />
- <meta name="author" content="Jeremy Ward" />
- <meta name="robots" content="noindex,nofollow" />
- <meta name="viewport" content="width=device-width, initial-scale=1" />
- <link rel="stylesheet" href="css/style.css" />
- <link rel="stylesheet" href="css/form.css" />
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
- <script src='js/main.js'></script>
+  <!-- Meta information we are also using a cdn to get jquery-->
+  <title> <?=$title?></title>
+  <meta charset="utf-8" />    
+  <meta name="description" content="Jeremy Wards Portal For IT162" />
+  <meta name="author" content="Jeremy Ward" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/form.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src='js/main.js'></script>
 </head>
 <body>

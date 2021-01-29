181b9410d635bef3a9f0ac7bd85cfbadca80ac04
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-29 14:27:58 -0800

    2

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <header id="header">
-        <h1><i class="logo fa fa-home"></i>
+        <h1><i class="logo fa <?=$logo?>"></i>
             <?php echo $title ?></sub>
         </h1>
     <!-- Begin Nav-->

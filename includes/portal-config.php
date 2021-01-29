0032b2c3b505a65a6eecccba6f389e01e13be2a6
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-29 13:39:17 -0800

    updates

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -5,8 +5,25 @@ jeremy ward 1/29/21
 portal-config.php
 this holds all of our config information for it 162
 */
+
 ob_start();
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 
-echo THIS_PAGE;
+//echo THIS_PAGE;
+switch(THIS_PAGE) {
+    case 'index.php' : 
+        $title = "Jeremy's IT162 Portal Page";
+        $logo = 'fa-home';
+        $pageID = 'Welcome';
+    break;
+    case 'contactme.php':
+        $title = "Jeremy's Contact Page";
+        $logo = 'fa-pencil-square-o';
+        $PageID = 'Contact Jeremy';
+    break;
+
+
+
+    default: $title = THIS_PAGE;
+}
 ?>

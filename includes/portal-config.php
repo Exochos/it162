4a9a1c269eb813ab48015901c69a7ad55f01d1cb
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-29 11:06:58 -0800

    Update portal-config.php

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -6,11 +6,7 @@ portal-config.php
 this holds all of our config information for it 162
 */
 ob_start();
-//echo basename($_SERVER['PHP_SELF']);
 define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 
 echo THIS_PAGE;
-//echo $_SERVER['PHP_SELF'];
-//die;
-
 ?>

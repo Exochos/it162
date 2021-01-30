a8d698fd25f915ced2fd3906e1e02d95368f8e91
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-29 16:41:19 -0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -20,14 +20,23 @@
         <h1><i class="logo fa <?=$logo?>"<?=$logo_color?>></i>
             <?php echo $title ?></sub>
         </h1>
-    <!-- Begin Nav-->
+    
     <nav class="topnav" id="myTopnav">
+
+      <?=makeLinks($nav1)?>   
+    <!-- Comment out
         <a href="index.php" class='active'>Welcome</a>
         <a href="big/index.html">BIG</a>
         <a href="aia.html">AIA</a>
         <a href="flowchart.html">FlowChart</a>
         <a href="fp/index.html">Final Project</a>
         <a href="contact.php"><i class="fa fa-envelope-open-o" aria-hidden="true"></i> Contact Jeremy</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars"></i></a>
+        End comment out nav -->
+
+
+
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars"></i>&#9776;</a>
     </nav>
+    
+
 </header>

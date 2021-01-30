75b49c2c12f30df0898dfc15168bc9907af0097b
Exochos <35015441+Exochos@users.noreply.github.com>
2021-01-29 16:41:53 -0800

    Update portal-config.php

--- includes/portal-config.php
+++ includes/portal-config.php
@@ -13,6 +13,16 @@ define('THIS_PAGE',basename($_SERVER['PHP_SELF']));
 date_default_timezone_set('America/Los_Angeles');
 $logo_color = '';
 
+//place URL & labels in the array here for navigation:
+
+    $nav1['index.php'] = "Welcome";
+    $nav1['big/index.php'] = "Big";
+    $nav1['aia.php'] = "AIA";
+    $nav1['flowchart.php'] = "Flowchart";
+    $nav1['fp/index.php'] = "Final Project";
+    $nav1['contact.php'] = "Contact Jeremy";
+    
+
 switch(THIS_PAGE) {
 
     case 'index.php' : 
@@ -40,4 +50,25 @@ switch(THIS_PAGE) {
     $PageID = 'Welcome';
 
 }
+
+/*
+makeLinks function will create our dynamic nav when called.
+Call like this:
+echo makeLinks($nav1); #in which $nav1 is an associative array of links
+*/
+
+function makeLinks($linkArray) {
+    $myReturn = '';
+    foreach($linkArray as $url => $text) {
+        if($url == THIS_PAGE)
+        {//selected page - add class reference
+	    	$myReturn .= '<li><a class="selected" href="' . $url . '">' . $text . '</a></li>' . PHP_EOL;
+    	}else{
+	    	$myReturn .= '<li><a href="' . $url . '">' . $text . '</a></li>'  . PHP_EOL;
+    	}    
+    }
+      
+    return $myReturn;    
+}
+
 ?>

0c9d21ea140ce7bef35ee6fbb58c04f5bcecd779
Exochos <35015441+Exochos@users.noreply.github.com>
2021-02-20 17:25:40 -0800

    flexbox

--- big/flexbox.php
+++ big/flexbox.php
@@ -5,17 +5,25 @@
 <div class="container" style="margin-top:30px">
   <div class="row">
     <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2017</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2>Flexbox Explained </h2>
+      <img src="https://i1.wp.com/www.silocreativo.com/en/wp-content/uploads/2017/04/flexbox-cssgrid-practical-example.png?fit=666%2C370&quality=100&strip=all&ssl=1">
+      <p>As pages needed to get more responsive in order to better serve the multitude of screen sizes in current use around the globe, it was realized that the current top down design scheme would need some flexibility. Two different standards have been put forward, Grid and Flexbox. We will be examining Flexbox in this paper.
+         “The Flexbox Layout (Flexible Box) module (a W3C Candidate Recommendation as of October 2017) aims at providing a more efficient way to lay out, align and distribute space among items in a container, even when their size is unknown and/or dynamic (thus the word “flex”).”</p>
+         <img src="https://i1.wp.com/www.silocreativo.com/en/wp-content/uploads/2017/04/flexbox-cssgrid-practical-example.png?fit=666%2C370&quality=100&strip=all&ssl=1"><br>
+         <p>“The main idea behind the flex layout is to give the container the ability to alter its items’ width/height (and order) to best fill the available space (mostly to accommodate to all kind of display devices and screen sizes). A flex container expands items to fill available free space or shrinks them to prevent overflow.”</p>
       <br>
+      
     </div>
         <div class="col-sm-4">
-      <h2>About Me</h2>
-      <h5>Photo of me:</h5>
-
+    
+      <h3>Resources & Citations:</h3>
+        <cite>
+        <ul>
+            <li>        “A Complete Guide to Flexbox: CSS-Tricks.” CSS, 21 Dec. 2020,<br> <a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">css-tricks.com/snippets/css/a-guide-to-flexbox/ </a></li>
+            <li>Pillar, Miroslav. “A Comprehensive Guide to Flexbox.” Medium, Better Programming, 8 Apr. 2020, <br><a href="https://medium.com/better-programming/a-comprehensive-guide-to-flexbox-ca0ffdf770c6">medium.com/better-programming/a-comprehensive-guide-to-flexbox-ca0ffdf770c6</a> </li>
+            <li>“Web Technology for Developers.” CSS: Cascading Style Sheets | MDN,<br> <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox</a></li>
+    </ul>
+    </cite>
     </div>
   </div>
 </div>
--- big/includes/header.php
+++ big/includes/header.php
@@ -22,7 +22,7 @@
 </div>
 
 <!-- Bootstrap navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light" style="position:sticky;">
  <a class="navbar-brand" href="../index.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>

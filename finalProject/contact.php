08f1a81fb7605bfa8dca0c078a07234735ec0407
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-22 13:40:06 -0700

    Update contact.php

--- finalProject/contact.php
+++ finalProject/contact.php
@@ -15,11 +15,11 @@
   <form action="?" method="POST">
   
     <label for="name">Name:</label><br>
-    <input type="text" id="name" placeholder="Write your name here.."> </input><br>
+    <input type="text" id="name" placeholder="Write your name here.."><br>
     <label for="phone">Phone Number:</label><br>
-    <input type="tel" id="phone" placeholder="Phone Number"></input><br>
+    <input type="tel" id="phone" placeholder="Phone Number"><br>
     <label for="email">Email:</label><br>
-    <input type="email" id="email" placeholder="Email"> </input><br>
+    <input type="email" id="email" placeholder="Email"><br>
     <label for="message">Message:</label><br>
     <textarea type="text" id="message" rows="6" cols="50"></textarea><br>
     <div class="g-recaptcha" data-sitekey="6LcEQ4kaAAAAAMlQrU2brFzS7kPRrGxE-a9t9-1t"></div><br>

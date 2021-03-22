7d58a4c67a7999f5e10c6a7d6724e55c1713bc61
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-22 13:21:58 -0700

    Update about.php

--- finalProject/about.php
+++ finalProject/about.php
@@ -8,12 +8,8 @@
 
 
 <!-- Main Content Goes Here -->
-<div class="muted">&nbsp;</div>
-<video autoplay muted loop id="myVideo" id="myVideo">
-  <source src="images/video2.mp4" type="video/mp4">
-</video>
 <main>
-<div style="font-family: 'Dancing Script', cursive;font-size: 60px;">About Celeste</div>
+<div class="subheader"><?php echo $title; ?></div>
 
 <p style="font-family: ariel;">
 The standard Lorem Ipsum passage, used since the 1500s
@@ -32,9 +28,6 @@ The standard Lorem Ipsum passage, used since the 1500s
 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 </p>
 </main>
-<scipt>
-    
-</script>
 <!-- End Main Content -->
 
 <?php

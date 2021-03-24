a7e2fb99302af34c00ac3fefb5d7b8f1637f2aba
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-24 14:26:00 -0700

    Update index.php

--- finalProject/index.php
+++ finalProject/index.php
@@ -11,13 +11,8 @@
 <main>
 <div class="subheader"><?php echo $title; ?></div>
 <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue quam sed pulvinar posuere. Fusce vitae tortor risus. Nam condimentum faucibus lorem ut ultricies. Sed ac magna tellus. Vestibulum et risus ullamcorper, volutpat odio nec, condimentum nibh. Mauris finibus congue mauris eu placerat. In in orci sed eros porttitor sagittis ut non erat. Ut hendrerit felis nisi, eleifend dictum ex semper a. Sed faucibus ornare sem non ultrices. Maecenas id neque et diam cursus egestas a ac ex. Nullam interdum commodo ante, sed tempor arcu efficitur at. Pellentesque in leo tellus. Sed non lorem libero.
-</p>
-<p>
-Donec ut malesuada lacus. Aenean varius in massa in bibendum. Morbi ut molestie nunc. In sed imperdiet dui. Mauris sed auctor erat. Maecenas at massa eget mauris fermentum commodo. Donec pellentesque turpis a nulla posuere tristique. Vivamus vestibulum ut erat sit amet tempor. Nam euismod, ipsum eget volutpat viverra, lectus nibh iaculis lorem, vel viverra elit quam at ex. Pellentesque sit amet tempus nulla. Proin quam urna, pellentesque sed varius eget, varius sed est.
-</p>
-<p>
-Nullam tristique pulvinar malesuada. Sed commodo rhoncus ante, et pharetra ligula bibendum eu. Sed at elit vel nisl tincidunt vestibulum. Ut pharetra ornare lectus eget placerat. Integer vel fringilla quam, at hendrerit eros. Duis rhoncus eleifend feugiat. Nulla facilisi. Cras dictum, ante vitae iaculis auctor, lorem dolor convallis urna, a elementum diam enim vestibulum metus. Quisque ante justo, luctus in mauris at, elementum feugiat arcu. Donec vehicula facilisis est nec fermentum.
+I coach people within a safe space to unlock their full potential and break down their barriers. We get to the root of the way that you are programmed, and I help you take charge of reprogramming yourself and your narrative to achieve the future you choose. I ask you the direct powerful questions that get you to the answers you already have inside of you.<br>
+The space I create in coaching is a playful one. There are no wrong answers here - just a playground where you have the ability to look inside yourself and see what’s there. We pull out what’s useful and discard the rest. <br>
 </p>
 </main>
 <!-- End Main Content -->

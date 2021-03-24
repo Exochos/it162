1a3df9ab3f61ded80face3c2a12de6eb7364b1aa
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-24 14:29:14 -0700

    Update index.php

--- finalProject/index.php
+++ finalProject/index.php
@@ -12,7 +12,7 @@
 <div class="subheader"><?php echo $title; ?></div>
 <p>
 
-<img src="images/IMG_20200112_152632.jpg" alt="celeste">
+<img src="images/IMG_20200112_152632.jpg" alt="celeste" width="400" height="400">
 I coach people within a safe space to unlock their full potential and break down their barriers. We get to the root of the way that you are programmed, and I help you take charge of reprogramming yourself and your narrative to achieve the future you choose. I ask you the direct powerful questions that get you to the answers you already have inside of you.<br>
 The space I create in coaching is a playful one. There are no wrong answers here - just a playground where you have the ability to look inside yourself and see what’s there. We pull out what’s useful and discard the rest. <br>
 

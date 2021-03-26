511fa66cfe26a81eab4e8a0b93176b3f4fc76441
Exochos <35015441+Exochos@users.noreply.github.com>
2021-03-25 19:36:32 -0700

    asd

--- finalProject/includes/footer.php
+++ finalProject/includes/footer.php
@@ -6,7 +6,7 @@
 <!-- End Container -->
 <!-- Footer -->
 <footer class="footer">
-  <div><i class="fa fa-facebook-f"></i></div><div><i class="fa fa-instagram"></i></div><div><i class="fa fa-twitter"></i></div>
+  <div><a href=""><i class="fa fa-facebook-f"></i></a></div><div><a href=""><i class="fa fa-instagram"></i></a></div><div><a href=""><i class="fa fa-twitter"></i></a></div>
   <div class="spacer">&nbsp;</div>
   <div>
     <small> &copy;2020-<?=date('Y')?> by <a href="https://github.com/Exochos" target="_blank"><i class="fa fa-github"></i>Jeremy Ward</a><a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></div>
